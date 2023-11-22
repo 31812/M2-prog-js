fabbc972daaafa1e3c8c06a9d9e10d2378cce2fd
31812 <31812@ma-web.nl>
2023-11-22 15:46:04 +0100

    01A

--- 01A/app.js
+++ 01A/app.js
@@ -3,7 +3,20 @@ class App
     runApplication()
     {
         console.log("hello world!")
-
+        let Appnaam = "DemoApp"
+        let versienummer = 0.1
+        let versiedatum = "Wed 22 Nov 2023"
+        let autheur = "student"
+        let copyright = "Elias Reifen"
+        let distrubuteur = "Angelo Struiken"
+        let darkmode = "false"
+        console.log("Appnaam:", Appnaam)
+        console.log("Versienummer:", versienummer)
+        console.log("Versiedatum:", versiedatum)
+        console.log("Autheur:", autheur)
+        console.log("Copyright:", copyright)
+        console.log("Distrubuteur:", distrubuteur)
+        console.log("Darkmode:", darkmode)
     }
 }
 
