c126f921b5910635d5e0be16e32cb33f82ff32e6
31812 <31812@ma-web.nl>
2023-12-05 13:04:44 +0100

    app

--- 01C/app.js
+++ 01C/app.js
@@ -8,14 +8,12 @@ class App {
         this.distrubuteur = "Angelo Struiken";
         this.darkmode = false;
 
-        // Class variables
         this.classBool = true;
         this.classString = "Class String";
         this.classNumber = 42;
     }
 
     runApplication() {
-        // Local variables
         let localBool = false;
         let localString = "Local String";
         let localNumber = 21;
@@ -28,13 +26,11 @@ class App {
         console.log("Copyright:", this.copyright);
         console.log("Distrubuteur:", this.distrubuteur);
         console.log("Darkmode:", this.darkmode);
-
-        // Logging local variables
+        
         console.log("Local Boolean:", localBool);
         console.log("Local String:", localString);
         console.log("Local Number:", localNumber);
 
-        // Logging class variables
         console.log("Class Boolean:", this.classBool);
         console.log("Class String:", this.classString);
         console.log("Class Number:", this.classNumber);
