9c69ad63cba8827283d9399217c1348380f06a2c
31812 <31812@ma-web.nl>
2023-12-05 14:18:06 +0100

    canvas

--- 02 canvashuis/app.js
+++ 02 canvashuis/app.js
@@ -6,6 +6,9 @@ class App
         console.log("hello world!")
         let canvas = document.getElementById("canvasid")
         let g = canvas.getContext("2d");
+        g.fillStyle = "#33FF56";
+        g.fillRect(0,0,canvas.width,canvas.height);
+        g.fillStyle = "#6A33FF";
         g.fillRect(0,0,10,10);
         console.log(canvas)
     }
