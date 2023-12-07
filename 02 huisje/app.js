f7f3b328d7bf4fd79d4caa654858d8a9051e1b7d
31812 <31812@ma-web.nl>
2023-12-07 14:28:22 +0100

    HuisjeKleiner

--- 02 huisje/app.js
+++ 02 huisje/app.js
@@ -43,8 +43,50 @@ class App
         g.fill()
         console.log(canvas)
     }
+    runApplicationKleiner()
+    { 
+        console.log("hello world!")
+        let canvas = document.getElementById("canvasid")
+        let g = canvas.getContext("2d");
+        g.fillStyle = "#FF0000";
+        g.beginPath()
+        g.moveTo(80, 30);
+        g.lineTo(70,20);
+        g.lineTo(60, 40); 
+        g.closePath();
+        g.stroke(); 
+        g.fill()
+        g.fillStyle = "#FF0000";
+        g.beginPath()
+        g.moveTo(70, 20);
+        g.lineTo(30,10);
+        g.lineTo(20, 30);
+        g.lineTo(60, 40);
+        g.closePath();
+        g.stroke(); 
+        g.fill()
+        g.fillStyle = "#FFFFFF";
+        g.beginPath()
+        g.moveTo(60, 40);
+        g.lineTo(60, 60);
+        g.lineTo(80, 50);
+        g.lineTo(80, 30);
+        g.closePath();
+        g.stroke(); 
+        g.fill()
+        g.fillStyle = "#FFFFFF";
+        g.beginPath()
+        g.moveTo(20, 30);
+        g.lineTo(20,50);
+        g.lineTo(60, 60);
+        g.lineTo(60, 40);
+        g.closePath();
+        g.stroke(); 
+        g.fill()
+        console.log(canvas)
+    }
 }
 
 let app = new App();
-app.runApplication();
+app.runApplicationKleiner();
 
