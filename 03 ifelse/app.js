55341d984297fcf33d6721c2369360ad338b653e
31812 <31812@ma-web.nl>
2023-12-08 14:03:11 +0100

    kleurtjes

--- 03 ifelse/app.js
+++ 03 ifelse/app.js
@@ -3,6 +3,35 @@ class App
     runApplication()
     {
         console.log("hello world!")
+        let title = document.getElementById("newstitle")
+        let newsitem1 = document.getElementsByClassName("gamenews headline")[0]
+        let newsitem2 = document.getElementsByClassName("gamenews")[1]
+        console.log(title)
+        // getElementById(newstitle) // 
+        // getElementByClassName("gamenews headline") // 
+        // getElementByClassName("gamenews") // 
+        let rand = Math.random();
+        console.log(rand);
+        if(rand < 0.2)
+        {
+            title.style.backgroundColor = "#FF0000";
+        } else {
+            title.style.backgroundColor = "#000000";
+        }
+
+        if(rand < 0.2)
+        {
+            newsitem2.style.backgroundColor = "#6EDA26";
+        } else {
+            newsitem2.style.backgroundColor = "#451769";
+        }
+
+        if(rand < 0.2)
+        {
+            newsitem1.style.backgroundColor = "#9CACBD";
+        } else {
+            newsitem1.style.backgroundColor = "9E22A8";
+        }
     }
 }
 
