efe21bdb9e0c3a406ec9c6e8363def64698fb386
31812 <31812@ma-web.nl>
2024-02-01 21:30:35 +0100

    07_bijna

--- 07 planten/app.js
+++ 07 planten/app.js
@@ -1,3 +1,10 @@
+class Plant{
+    constructor(naam,leeft){
+        this.naam = naam;
+        this.leeft = leeft;
+    }
+}
+
 class Dino{
     constructor(naam,vleeseter,leeftijd){
         this.naam = naam;
@@ -19,11 +26,13 @@ class App{
         console.log("Hello World")
         let dino = new Dino("Jerry",true,20);
         let plantenEter = new Dino("Cornelius",false,20);
+        let gras = new Plant("Gras",true);
+        let struik = new Plant("Struik",true);
         console.log(dino);
         console.log(plantenEter);
 
         dino.eatFood(plantenEter);
-        console.log("leeft " + plantenEter.naam + "? "+ plantenEter.leeft);
+        console.log("leef " + plantenEter.naam + "? "+ plantenEter.leeft);
     }
 }
 let app = new App();
---
+++ 07 this/app.js
@@ -0,0 +1,24 @@
+class User
+{
+    constructor(name){
+        this.naamVanDeEigenschap = "Phaparushca"
+    }
+}
+
+class App
+{
+
+    runApplication(){
+        console.log("hello world!")
+
+        let user1 = username1
+        let user2 = username2
+        let user3 = username3
+        let user4 = username4
+        let user5 = username5
+        let user6 = username6
+    }
+}
+
+let app = new App()
+app.runApplication()
