31cad756878394d0aa41435ad156ef04033af54d
31812 <31812@ma-web.nl>
2024-02-01 12:55:50 +0100

    06_function

---
+++ 06 function/app.js
@@ -0,0 +1,14 @@
+class App {
+    runApplication() {
+        console.log("hello world!");
+        let uiButton = document.getElementById("myButton");
+
+        uiButton.addEventListener("click",(e)=>
+        {
+            console.log("click!");
+        });
+    }
+}
+
+let app = new App();
+app.runApplication();
